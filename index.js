3985e7ae72059bec8d4513ecb8a015ef93675b58
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-08-12 16:14:54 +0700

    I think I finish i finish new post system let's do CSS

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+var postData = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", {"data": postData});
 });
 
 app.post("/submit", (req, res) => {
-    res.render("index.ejs")
+
+    const postTitle = req.body['post-header'];
+
+    if (req.body['post-header'] && typeof postTitle === 'string') {
+        postData.push(postTitle);
+    }
+
+    console.log(postData)
+
+    res.render("index.ejs", {"data": postData});
 });
 
 app.listen(port, () => {
